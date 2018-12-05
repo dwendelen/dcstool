49565f6da5ae20715a51f4f13cda16073dfe55cf
Daan Wendelen <daanwendelen@gmail.com>
2018-12-05 15:58:42 +0100

    No more sleep mode on inactivity

--- app/src/main/java/se/daan/dcstool/ui/MainActivity.kt
+++ app/src/main/java/se/daan/dcstool/ui/MainActivity.kt
@@ -3,11 +3,13 @@ package se.daan.dcstool.ui
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main)
 
         val mainPagerAdapter = MainPagerAdapter(supportFragmentManager)
